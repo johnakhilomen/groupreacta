9f7911eac5aafa2f20db7223a7c26f0ceaaf7fba
John Akhilomen <jonbasanti@gmail.com>
2020-11-21 11:54:39 -0600

    Msg

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+{/* extend the Component class to bring in in-built functions and 
+properties from React Component class */}
+class Header extends Component {
+    state = {  }
+    render() { 
+        return ( 
+        <div>
+            <h1>First React Group Project</h1>
+        </div> );
+    }
+}
+ 
+export default Header;
